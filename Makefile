3ce8f948e2f0eceab80f8185da53705e47e6c987
Sébastien Millet <milletseb@laposte.net>
2022-09-20 21:46:54 +0200

    Switches from arduino-builder to arduino-cli

--- Makefile
+++ Makefile
@@ -1,10 +1,16 @@
-ARDUINO_DIR = /usr/share/arduino
-ARDUINO_LIBS =
-ARDMK_DIR = /home/sebastien/.arduino_mk
+ALL:
 
-# USER_LIB_PATH = /home/sebastien/travail/cpp/seb/arduino/libraries
+clean:
+	make -C examples/01_send/ $@
+	make -C examples/02_send_otio/ $@
+	make -C examples/03_send_short/ $@
+	make -C examples/04_rcswitch_send/ $@
+	make -C examples/05_send_20220829083700/ $@
 
-BOARD_TAG = uno
-MCU = atmega328
+mrproper:
+	make -C examples/01_send/ $@
+	make -C examples/02_send_otio/ $@
+	make -C examples/03_send_short/ $@
+	make -C examples/04_rcswitch_send/ $@
+	make -C examples/05_send_20220829083700/ $@
 
-include $(ARDMK_DIR)/Arduino.mk
---
+++ examples/05_send_20220829083700/05_send_20220829083700.ino
@@ -0,0 +1,80 @@
+// 05_send_20220829083700.ino
+
+// Simple example of sending codes with a Radio Frequencies device.
+// Sends code 4 times every 5 seconds.
+
+/*
+  Copyright 2021 Sébastien Millet
+
+  `RF433send' is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
+
+  `RF433send' is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this program. If not, see
+  <https://www.gnu.org/licenses>.
+*/
+
+// Schematic:
+//   RF433 TRANSMITTER data pin plugged on Arduino D4
+
+#include "RF433send.h"
+
+#define PIN_RFOUT  4
+
+RfSend *tx_whatever;
+
+void setup() {
+    pinMode(PIN_RFOUT, OUTPUT);
+
+    Serial.begin(115200);
+
+        // rfsend_builder performs some asserts that, it failed, write details
+        // to Serial and then block execution.
+        // If construction is done before setup() execution (as is the case with
+        // global variables), no output is done and we loose interesting debug
+        // information.
+        // Therefore I prefer this style over using a global radio object.
+
+    tx_whatever = rfsend_builder(
+        RfSendEncoding::TRIBIT_INVERTED,
+        PIN_RFOUT,
+        RFSEND_DEFAULT_CONVENTION,  // Do we want to invert 0 and 1 bits? No.
+        6,       // Number of sendings
+        nullptr, // No callback to keep/stop sending (if you want to send
+                 // SO LONG AS a button is pressed, the function reading the
+                 // button state is to be put here).
+        8550,    // initseq
+        0,       // lo_prefix
+        0,       // hi_prefix
+        500,     // first_lo_ign
+        500,     // lo_short
+        500,     // lo_long
+        1900,    // hi_short
+        3800,    // hi_long
+        0,       // lo_last
+        8550,    // sep
+        37       // nb_bits
+    );
+
+}
+
+byte data[] = { 0x1f, 0x03, 0x14, 0x15, 0x93 };
+void loop() {
+    static int count = 0;
+
+    byte n = tx_whatever->send(sizeof(data), data);
+    Serial.print("Envoi effectué ");
+    Serial.print(n);
+    Serial.print(" fois\n");
+
+    delay(5000);
+}
+
+// vim: ts=4:sw=4:tw=80:et
